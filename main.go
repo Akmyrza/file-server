cf084b065ba729de3681cacd4257a89bde41c858
Akmyrza <onlyfordox@mail.ru>
2022-06-21 18:00:09 +0600

    changed

--- main.go
+++ main.go
@@ -21,14 +21,15 @@ func main() {
 func uploadFile(ctx *gin.Context) {
 	file, err := ctx.FormFile("file")
 	if err != nil {
-		log.Println(err)
-	} 
+		ctx.Error(err)
+	}
 
+	fmt.Print("asd")
 	path := makePath(file.Filename)
 
-	er := os.Mkdir(path, os.ModePerm)
+	er := os.MkdirAll(path, os.ModePerm)
 	if er != nil {
-		log.Println(er)
+		ctx.Error(er)
 	}
 
 	ctx.SaveUploadedFile(file, path)
@@ -39,7 +40,9 @@ func makePath(filename string) string {
 	year := time.Now().Year()
 	month := time.Now().Month().String()
 	day := time.Now().Day()
+	//currentTime := time.Now()
 	return strconv.Itoa(year) + "/" + month + "/" + strconv.Itoa(day) + "/" + filename
+	//	return string(currentTime.Format("2000/01/31"))
 }
 
 func downloadFile(ctx *gin.Context) {
@@ -48,6 +51,6 @@ func downloadFile(ctx *gin.Context) {
 	if err != nil {
 		log.Println(err)
 	}
-	defer file.Close()	
+	defer file.Close()
 	ctx.File(path)
 }
